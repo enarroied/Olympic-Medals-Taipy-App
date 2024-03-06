4ad5987b0919b4cf92856187faddbcd024a111c6
Eric Narro <eric.narro@gmail.com>
2024-03-06 20:49:35 +0100

    Added general table and medal bar chart

--- src/main.py
+++ src/main.py
@@ -10,8 +10,96 @@ df_olympic_cities = pd.read_csv("./src/data/olympic_cities.csv")
 df_olympic_medals = pd.read_csv("./src/data/olympic_medals.csv")
 
 ###########################################################
+###             Ceate transformed DataFrames            ###
+###########################################################
+
+# Small DataFrame to display as summary table
+df_olympic_cities_simplified = df_olympic_cities[
+    [
+        "Olympiad",
+        "Olympic_year",
+        "Olympic_season",
+        "total_medals",
+        "total_medals_gold",
+        "total_medals_silver",
+        "total_medals_bronze",
+        "number_committees",
+        "number_disciplines",
+        "number_events",
+        "Country",
+        "Continent",
+    ]
+]
+
+# Define a custom sorting order for 'Medal_type'
+medal_order = {"Bronze": 0, "Silver": 1, "Gold": 2}
+
+df_medals_by_olympiad = (
+    df_olympic_medals.groupby(
+        ["Olympiad", "Olympic_year", "Medal_type", "Olympic_season"]
+    )
+    .size()
+    .reset_index(name="Medal_count")
+)
+
+# Sort the DataFrame first by 'Olympic_year' and then by 'Medal_type' using the custom sorting order
+df_medals_by_olympiad["Medal_type_code"] = df_medals_by_olympiad["Medal_type"].map(
+    medal_order
+)
+df_medals_by_olympiad = df_medals_by_olympiad.sort_values(
+    by=["Olympic_year", "Medal_type_code"]
+)
+
+# Reset index without creating a new column
+df_medals_by_olympiad.reset_index(drop=True, inplace=True)
+
+
+###########################################################
 ###                      Functions                      ###
 ###########################################################
+def create_bar_medals(df_medals_by_olympiad, season):
+    # Define colors for each medal type
+    medal_colors = {"Gold": "#FFD700", "Silver": "#C0C0C0", "Bronze": "#CD7F32"}
+
+    if season != "All":
+        df_medals_season = df_medals_by_olympiad[
+            df_medals_by_olympiad["Olympic_season"] == season
+        ].reset_index(drop=True)
+    else:
+        df_medals_season = df_medals_by_olympiad
+    # Create a stacked bar chart
+    fig = px.bar(
+        df_medals_season,
+        x="Olympiad",
+        y="Medal_count",
+        color="Medal_type",
+        color_discrete_map=medal_colors,  # Assign colors to medal types
+        title="Medal Count by Olympiad and Medal Type",
+        labels={"Medal_count": "Medal Count", "Olympiad": "Olympiad"},
+        category_orders={"Olympiad": df_medals_season["Olympiad"].unique()},
+    )
+    return fig
+
+
+###########################################################
+###                  Displayed objects                  ###
+###########################################################
+bar_medals = create_bar_medals(df_medals_by_olympiad, "All")
+
+###########################################################
+###         Initial variables and selector lists        ###
+###########################################################
+
+list_seasons = ["All", "summer", "winter"]
+
+season = "All"
+
+
+###########################################################
+###                  Selector Function                  ###
+###########################################################
+def on_selector(state):
+    state.bar_medals = create_bar_medals(df_medals_by_olympiad, state.season)
 
 
 ###########################################################
@@ -58,6 +146,20 @@ with tgb.Page() as page:
             )
             tgb.text("{int(len(df_olympic_medals))}", class_name="h3")
 
+    # Bar chart of all medals:
+    with tgb.layout("1 1"):
+        with tgb.part():
+            tgb.selector(
+                value="{season}",
+                lov=list_seasons,
+                dropdown=True,
+                label="Select season",
+                class_name="fullwidth",
+                on_change=on_selector,
+            )
+            tgb.chart(figure="{bar_medals}")
+
+    tgb.table("{df_olympic_cities_simplified}")
 ###########################################################
 ###                       Run App                       ###
 ###########################################################
