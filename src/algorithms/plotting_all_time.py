409d84117ec82e6edcf3c922c8a80d1f5dd10d30
enarroied <eric.narro.ied@gmail.com>
2025-06-18 22:23:41 +0200

    moved functions away

---
+++ src/algorithms/plotting_all_time.py
@@ -0,0 +1,184 @@
+import plotly.express as px
+
+
+def create_bar_medals(df_medals_by_olympiad, season):
+    """Creates a plotly bar chart with total olympic medals (broken by medal color).
+        The dataframe is previously filtered by season (summer / winter).
+
+    Args:
+        df_medals_by_olympiad (pd.DataFrame): data with all the olympic medals.
+        season (str): ""All", "winter" or "summer".
+
+    Returns:
+       px.bar: A plotly bar chart to display in the Taipy app
+    """
+    # Define colors for each medal type
+    medal_colors = {"Gold": "#FFD700", "Silver": "#C0C0C0", "Bronze": "#CD7F32"}
+
+    if season != "All":
+        df_medals_season = df_medals_by_olympiad[
+            df_medals_by_olympiad["Olympic_season"] == season
+        ].reset_index(drop=True)
+    else:
+        df_medals_season = df_medals_by_olympiad
+    # Create a stacked bar chart
+    fig = px.bar(
+        df_medals_season,
+        x="Olympiad",
+        y="Medal_count",
+        color="Medal_type",
+        color_discrete_map=medal_colors,  # Assign colors to medal types
+        title="Medal Count by Olympiad and Medal Type",
+        labels={"Medal_count": "Medal Count", "Olympiad": "Olympiad"},
+        category_orders={"Olympiad": df_medals_season["Olympiad"].unique()},
+    )
+    if season != "winter":
+        stockholm_annotation_y = 400  # Stockholm 1956 there are 18 medals total, but 400 will weep box out of othe chart elements
+        stockholm_annotation_x = (
+            df_medals_season[df_medals_season["Olympiad"] == "Stockholm 1956"].index[0]
+            / 3
+        ) + 1  # Divide by 3 because 3 types of medals
+
+        fig.add_annotation(
+            x=stockholm_annotation_x,
+            y=stockholm_annotation_y,
+            text=str("Stockholm 1956: only equestrian games"),
+            showarrow=True,
+            arrowhead=1,
+            arrowcolor="#FF0066",
+            arrowwidth=1,
+            arrowsize=1,
+            ax=0,
+            ay=-50,
+            font=dict(color="black", size=8),
+            align="center",
+            bordercolor="#c7c7c7",
+            borderwidth=2,
+            borderpad=4,
+            bgcolor=" #B0E0E6",
+            opacity=0.8,
+        )
+    return fig
+
+
+def create_bar_by_committee(df_medals, olympiad="All"):
+    """Creates a plotly bar chart with total olympic medals (medal colorS by each other).
+        The dataframe is previously filtered by season (summer / winter).
+
+    Args:
+        df_medals (pd.DataFrame): data with all the olympic medals.
+        olympiad (str): ""All", or name of the Olympiad.
+
+    Returns:
+       px.bar: A plotly bar chart to display in the Taipy app
+    """
+    df_medals_by_committee = df_medals.copy()
+
+    if olympiad != "All":
+        df_medals_by_committee = df_medals_by_committee[
+            df_medals_by_committee["Olympiad"] == olympiad
+        ]
+
+    # Define medal colors
+    medal_colors = {"Gold": "#FFD700", "Silver": "#C0C0C0", "Bronze": "#CD7F32"}
+
+    # Aggregating data to get count of medals by Medal_type for each Committee
+    df_aggregated = (
+        df_medals_by_committee.groupby(["Committee", "Medal_type"])
+        .size()
+        .unstack(fill_value=0)
+    )
+
+    # Sort DataFrame by count of gold and silver medals
+    df_aggregated = df_aggregated.sort_values(by=["Gold", "Silver"], ascending=False)
+
+    # Plotly bar chart
+    fig = px.bar(
+        df_aggregated,
+        x=df_aggregated.index,
+        y=["Gold", "Silver", "Bronze"],
+        barmode="group",
+        orientation="v",
+        color_discrete_map=medal_colors,
+        labels={"value": "Count", "variable": "Medal Type"},
+        title="Count of Gold, Silver, Bronze Medals by Committee",
+    )
+    fig.update_layout(xaxis={"title": "Committee"}, yaxis={"title": "Count"})
+    return fig
+
+
+def plot_olympic_medals_by_country(df_olympic_cities, season, medal_type):
+    """
+    Plot a choropleth map of Olympic medals by country for a specified season and medal type.
+
+    Args:
+        df_olympic_medals (pandas.DataFrame): DataFrame containing Olympic medals data.
+        season (str): The season for which to plot the medals. Should be either "winter" or "summer".
+        medal_type (str): The type of medal to count. Should be one of "All", "Gold", "Silver", or "Bronze".
+
+    Returns:
+        None
+    """
+    if medal_type == "All":
+        medal_column = "total_medals"
+    elif medal_type == "Gold":
+        medal_column = "total_medals_gold"
+    elif medal_type == "Silver":
+        medal_column = "total_medals_silver"
+    elif medal_type == "Bronze":
+        medal_column = "total_medals_bronze"
+    else:
+        raise ValueError(
+            "Invalid medal_type. Should be one of 'All', 'Gold', 'Silver', or 'Bronze'."
+        )
+
+    if season != "All":
+        df_olympic_cities = df_olympic_cities[
+            df_olympic_cities["Olympic_season"] == season
+        ]
+
+    country_counts = (
+        df_olympic_cities.groupby(["Country", "ISO_code_mapping"])[medal_column]
+        .sum()
+        .reset_index(name="Number of Medals")
+    )
+
+    fig = px.choropleth(
+        country_counts,
+        locations="ISO_code_mapping",
+        color="Number of Medals",
+        hover_name="Country",
+        color_continuous_scale=px.colors.sequential.Plasma,
+        title=f"{medal_type.capitalize()} Olympic Medals awarded by Host Country ({season.capitalize()})",
+        projection="natural earth",
+    )
+
+    fig.update_geos(
+        showcountries=True,
+        showland=True,
+        landcolor="lightgray",
+        countrycolor="white",
+    )
+
+    return fig
+
+
+def create_sunburnst_medals(df_olympic_medals, selected_olympiad_for_sunburst):
+    gender_category_colors = {
+        "Men": "#6baed6",  # Light blue
+        "Women": "#fb6a4a",  # Light red
+        "Open": "#74c476",  # Light green
+        "Mixed": "#9e9ac8",  # Light purple
+    }
+    if selected_olympiad_for_sunburst != "All":
+        df_olympic_medals = df_olympic_medals[
+            df_olympic_medals["Olympiad"] == selected_olympiad_for_sunburst
+        ]
+    fig = px.sunburst(
+        df_olympic_medals,
+        path=["Gender", "Discipline", "Event"],
+        color="Gender",
+        color_discrete_map=gender_category_colors,
+        title=f"Total Medals by Gender, Discipline, and Event - {selected_olympiad_for_sunburst}",
+    )
+    return fig
