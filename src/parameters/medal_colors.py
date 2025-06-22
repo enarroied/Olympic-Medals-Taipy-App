d763c28c698716c1d31c694b3f9a34acd47458b1
enarroied <eric.narro.ied@gmail.com>
2025-06-22 16:10:31 +0200

    added medal_color dataclass

---
+++ src/parameters/medal_colors.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+
+@dataclass
+class MedalColorMap:
+    gold: str = "#FFD700"
+    silver: str = "#C0C0C0"
+    bronze: str = "#CD7F32"
+
+    def as_dict(self):
+        return {
+            "Gold": self.gold,
+            "Silver": self.silver,
+            "Bronze": self.bronze
+        }
