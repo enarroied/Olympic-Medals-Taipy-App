df8212dbad05e8ba380d07b558f7840446e770f6
enarroied <eric.narro.ied@gmail.com>
2025-10-27 08:21:00 +0100

    cleaned mega fucntion

--- src/algorithms/create_medal_by_olympic_and_discipline.py
+++ src/algorithms/create_medal_by_olympic_and_discipline.py
@@ -8,6 +8,8 @@ and by Olympic event.
 Used by `all_time_medals.py`.
 """
 
+from functools import partial
+
 import pandas as pd
 import plotly.express as px
 
@@ -21,6 +23,21 @@ class MedalsByOlympicAndDiscipline:
     ):
         """Initialize MedalsBySeason with medal data and optional color mapping."""
         self.df_olympic_medals = df_olympic_medals.copy()
+        self.df_summer = self._filter_olympic_season(df_olympic_medals, "summer")
+        self.df_winter = self._filter_olympic_season(df_olympic_medals, "winter")
+        self.summer_disciplines = self.df_summer["Discipline"].unique()
+        self.winter_disciplines = self.df_winter["Discipline"].unique()
+        # Bind convenience callables that only require committee
+        # Note: self._create_medals_grid_core is a bound method; partial binds the next args.
+        self._create_summer_grid = partial(
+            self._create_medals_grid, self.df_summer, self.summer_disciplines
+        )
+        self._create_winter_grid = partial(
+            self._create_medals_grid, self.df_winter, self.winter_disciplines
+        )
+
+    def _filter_olympic_season(self, df, season):
+        return df[(df["Olympic_season"] == season)].copy()
 
     def _plot_grid_for_country(self, df_grouped, title, ordered_olympiads):
         fig = px.imshow(
@@ -40,61 +57,42 @@ class MedalsByOlympicAndDiscipline:
         )
         return fig
 
-    def create_medals_grid(self, committee, season):
-        """
-        Plot medals won by a committee across different disciplines and Olympiads.
-
-        Parameters:
-        - df_medals (DataFrame): DataFrame containing medal data.
-        - committee (str): Name of the committee.
-        - season (str): Olympic season: "summer" or "winter".
-
-        Returns:
-        - fig: Plotly figure object showing medals by Olympiad and discipline for
-        the committee.
-        """
-        df_medals = self.df_olympic_medals.copy()
-        df_filtered = df_medals[(df_medals["Olympic_season"] == season)]
-
-        # Get all possible disciplines --> Like this, all disciplines appear for all
-        # countries
-        # Important to do this after filtering by season and before filtering by
-        # committee!
-        all_disciplines = df_filtered["Discipline"].unique()
-
-        # And then only filter the DataFrame by committee
-        df_filtered = df_filtered[(df_filtered["Committee"] == committee)]
-        df_grouped = df_filtered.pivot_table(
+    def _pivot_olympic_by_discipline(self, df):
+        return df.pivot_table(
             index=["Olympiad", "Olympic_year"],
             columns="Discipline",
             aggfunc="size",
             fill_value=0,
             observed=True,
-        )
+        ).sort_index(level=1)
 
-        df_grouped = df_grouped.sort_index(level=1)
+    def _create_medals_grid(self, df_medals, all_disciplines, committee):
+        """
+        Plot medals won by a committee across different disciplines and Olympiads.
 
-        # TODO: take this out of this function!
-        ordered_olympiads = list(df_grouped.index.get_level_values("Olympiad").unique())
+        Parameters:
+        - df_medals (DataFrame): DataFrame containing medal data.
+        - all_disciplines (list): All the disciplines for the Olympic season.
+        - committee (str): Name of the committee.
 
-        # Add all the disciplines of the selcted season, whether the Committee won a
-        #  medals or not
-        return (
-            df_grouped.reindex(columns=all_disciplines, fill_value=0),
-            ordered_olympiads,
+        Returns:
+        - fig: Plotly figure object showing medals by Olympiad and discipline for
+        the committee.
+        """
+        df_medals = df_medals[(df_medals["Committee"] == committee)]
+        return self._pivot_olympic_by_discipline(df_medals).reindex(
+            columns=all_disciplines, fill_value=0
         )
 
     # TODO: Add a grouped abstraction to reduce repetition
     def plot_medals_grid_summer(self, committee):
-        df_grouped, ordered_olympiads = self.create_medals_grid(
-            committee=committee, season="summer"
-        )
+        df_grouped = self._create_summer_grid(committee=committee)
+        ordered_olympiads = list(df_grouped.index.get_level_values("Olympiad").unique())
         title = f"Medals by Olympiad and discipline for {committee} | summer"
         return self._plot_grid_for_country(df_grouped, title, ordered_olympiads)
 
     def plot_medals_grid_winter(self, committee):
-        df_grouped, ordered_olympiads = self.create_medals_grid(
-            committee=committee, season="winter"
-        )
+        df_grouped = self._create_winter_grid(committee=committee)
+        ordered_olympiads = list(df_grouped.index.get_level_values("Olympiad").unique())
         title = f"Medals by Olympiad and discipline for {committee} | winter"
         return self._plot_grid_for_country(df_grouped, title, ordered_olympiads)
