f29684fcbaac2c7ecdbb820b3126a02527485be0
enarroied <eric.narro.ied@gmail.com>
2025-10-27 23:18:18 +0100

    cleaned function total medals

--- src/algorithms/callbacks.py
+++ src/algorithms/callbacks.py
@@ -15,20 +15,13 @@ def on_selector_medals_by_committee(state):
         ) = create_medals_detail(df_grouped_medals_olympics, selected_committe)
 
 
-def _count_medals(df_olympic_medals, medal_type):
-    return int(
-        df_olympic_medals[df_olympic_medals["Medal_type"] == medal_type][
-            "Medal_type"
-        ].count()
-    )
+def _count_medals(df, medal_type):
+    return int((df["Medal_type"] == medal_type).sum())
 
 
 def init_total_medals(state):
     with state as s:
-        # Important to create new object:
-        s.medal_totals = MedalTotals(
-            int(len(s.df_olympic_medals)),
-            _count_medals(s.df_olympic_medals, "Gold"),
-            _count_medals(s.df_olympic_medals, "Silver"),
-            _count_medals(s.df_olympic_medals, "Bronze"),
-        )
+        df = s.df_olympic_medals
+        counts = {m.lower(): _count_medals(df, m) for m in ["Gold", "Silver", "Bronze"]}
+        print(counts)
+        s.medal_totals = MedalTotals(len(df), **counts)
